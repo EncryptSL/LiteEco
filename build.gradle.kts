d4e2a8deac0ab7b5a4a0a309b7bf6de66e55d0e1
EncryptSL <Rydlos123@gmail.com>
2022-10-09 21:30:24 +0200

    Bump new version for plugins and dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    id("io.papermc.paperweight.userdev") version "1.3.6"
+    id("io.papermc.paperweight.userdev") version "1.3.8"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    kotlin("jvm") version "1.7.10" apply true
+    kotlin("jvm") version "1.7.20" apply true
 }
 
 group = "encryptsl.cekuj.net"
@@ -22,14 +20,14 @@ repositories {
 
 dependencies {
     paperDevBundle(providers.gradleProperty("server_version").get())
-    compileOnly(kotlin("stdlib", "1.7.10"))
+    compileOnly(kotlin("stdlib", "1.7.20"))
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude("org.bukkit", "bukkit")
     }
     compileOnly("com.zaxxer:HikariCP:5.0.1")
     compileOnly("me.clip:placeholderapi:2.11.1")
-    implementation("cloud.commandframework:cloud-annotations:1.7.0")
-    implementation("cloud.commandframework:cloud-paper:1.7.0")
+    implementation("cloud.commandframework:cloud-annotations:1.7.1")
+    implementation("cloud.commandframework:cloud-paper:1.7.1")
     implementation("org.bstats:bstats-bukkit:3.0.0")
     testImplementation(kotlin("test"))
 }
@@ -48,10 +46,6 @@ tasks {
         filesMatching("plugin.yml") {
             expand(project.properties)
         }
-    }
-
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
     }
 
     reobfJar {
