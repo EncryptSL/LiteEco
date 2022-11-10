538580b98752c6903613b6cea61c49f289070c16
EncryptSL <Rydlos123@gmail.com>
2022-11-10 04:10:06 +0100

    Fixed position

--- src/main/kotlin/encryptsl/cekuj/net/extensions/TopPosition.kt
+++ src/main/kotlin/encryptsl/cekuj/net/extensions/TopPosition.kt
@@ -6,9 +6,14 @@ inline fun <K, V, M : Map<out K, V>> M.playerPosition(action: (index: Int, Map.E
 }
 
 inline fun <T, R> Iterable<T>.positionIndexed(transform: (index: Int, T) -> R): List<R> {
-    var index = 1
-    for (item in this) transform(index++, item)
     return mapIndexedTo(ArrayList<R>(collectionSizeOrDefault(10)), transform)
+}
+
+inline fun <T, R, C : MutableCollection<in R>> Iterable<T>.mapIndexedTo(destination: C, transform: (index: Int, T) -> R): C {
+    var index = 1
+    for (item in this)
+        destination.add(transform(index++, item))
+    return destination
 }
 
 fun <T> Iterable<T>.collectionSizeOrDefault(default: Int): Int = if (this is Collection<*>) this.size else default
