5b265187f7b18172e87fbc5b4fa49bbafc0ffd03
EncryptSL <Rydlos123@gmail.com>
2022-12-01 00:30:40 +0100

    Small refactoring of main class

--- src/main/kotlin/encryptsl/cekuj/net/LiteEco.kt
+++ src/main/kotlin/encryptsl/cekuj/net/LiteEco.kt
@@ -15,7 +15,7 @@ import encryptsl.cekuj.net.api.enums.TranslationKey
 import encryptsl.cekuj.net.commands.MoneyCMD
 import encryptsl.cekuj.net.config.TranslationConfig
 import encryptsl.cekuj.net.database.DatabaseConnector
-import encryptsl.cekuj.net.database.PreparedStatements
+import encryptsl.cekuj.net.database.models.PreparedStatements
 import net.milkbowl.vault.economy.Economy
 import org.bstats.bukkit.Metrics
 import org.bstats.charts.SingleLineChart
@@ -29,19 +29,26 @@ import java.util.function.Function
 
 class LiteEco : JavaPlugin() {
 
+    private val databaseConnector: DatabaseConnector by lazy { DatabaseConnector() }
     lateinit var metrics: Metrics
     lateinit var econ: Economy
     lateinit var updateNotifier: UpdateNotifier
-    lateinit var transactions: LinkedHashMap<String, Int>
+    var countTransactions: LinkedHashMap<String, Int> = LinkedHashMap()
     val pluginManger: PluginManager = server.pluginManager
-    val databaseConnector: DatabaseConnector by lazy { DatabaseConnector() }
-    val preparedStatements: PreparedStatements by lazy { PreparedStatements(this) }
+    val preparedStatements: PreparedStatements by lazy { PreparedStatements() }
     val translationConfig: TranslationConfig by lazy { TranslationConfig(this) }
     private val configLoaderAPI: ConfigLoaderAPI by lazy { ConfigLoaderAPI(this) }
     override fun onLoad() {
-        configLoaderAPI.setConfigName("database.db").load()
-            .setConfigName("config.yml").load()
-        translationConfig.loadTranslation()
+        configLoaderAPI
+            .create("database.db")
+            .create("config.yml")
+        translationConfig
+            .loadTranslation()
+        databaseConnector.initConnect(
+            config.getString("database.connection.jdbc_host")!!,
+            config.getString("database.connection.user")!!,
+            config.getString("database.connection.pass")!!
+        )
     }
 
     override fun onEnable() {
@@ -52,20 +59,13 @@ class LiteEco : JavaPlugin() {
             server.pluginManager.disablePlugin(this)
             return
         }
-        transactions = LinkedHashMap()
         metrics = Metrics(this, 15144)
         metrics.addCustomChart(SingleLineChart("transactions") {
-            transactions["transactions"]
+            countTransactions["transactions"]
         })
         updateNotifier = UpdateNotifier("101934", description.version)
         slF4JLogger.info(updateNotifier.checkPluginVersion())
         registerCommands()
-        databaseConnector.initConnect(
-            config.getString("database.connection.jdbc_host")!!,
-            config.getString("database.connection.user")!!,
-            config.getString("database.connection.pass")!!
-        )
-        preparedStatements.createTable(config.getString("database.connection.jdbc_host").toString())
         val handlerListeners = HandlerListeners(this)
         handlerListeners.registerListener()
         slF4JLogger.info("Plugin enabled in time ${System.currentTimeMillis() - start} ms")
