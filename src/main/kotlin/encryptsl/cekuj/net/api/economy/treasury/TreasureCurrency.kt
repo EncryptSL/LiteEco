07aff616fc2cee31bf3851fbd8906b52398c146e
EncryptSL <Rydlos123@gmail.com>
2023-01-13 15:47:44 +0100

    Added parser, in progress.

--- src/main/kotlin/encryptsl/cekuj/net/api/economy/treasury/TreasureCurrency.kt
+++ src/main/kotlin/encryptsl/cekuj/net/api/economy/treasury/TreasureCurrency.kt
@@ -43,7 +43,79 @@ class TreasureCurrency(private val liteEco: LiteEco) : Currency {
     }
 
     override fun parse(formatted: String, subscription: EconomySubscriber<BigDecimal>) {
-        TODO("Not yet implemented")
+        var valueBuilder = StringBuilder()
+        val currencyBuilder = StringBuilder()
+
+        var hadDot = false
+        for (c in formatted.toCharArray()) {
+            if (Character.isWhitespace(c)) {
+                continue
+            }
+            if (!Character.isDigit(c) && !isSeparator(c)) {
+                currencyBuilder.append(c)
+            } else if (Character.isDigit(c)) {
+                valueBuilder.append(c)
+            } else if (isSeparator(c)) {
+                if (c == decimal) {
+                    var nowChanged = false
+                    if (!hadDot) {
+                        hadDot = true
+                        nowChanged = true
+                    }
+                    if (!nowChanged) {
+                        valueBuilder = StringBuilder()
+                        break
+                    }
+                }
+                valueBuilder.append('.')
+            }
+        }
+
+        if (currencyBuilder.isEmpty()) {
+            subscription.fail(EconomyException(TreasuryFailureReasons.INVALID_CURRENCY))
+            return
+        }
+
+        val currency = currencyBuilder.toString()
+        if (!matches(currency)) {
+            subscription.fail(EconomyException(TreasuryFailureReasons.INVALID_CURRENCY))
+            return
+        }
+
+        if (valueBuilder.isEmpty()) {
+            subscription.fail(EconomyException(TreasuryFailureReasons.INVALID_VALUE))
+            return
+        }
+
+        try {
+            val value = valueBuilder.toString().toDouble()
+            if (value < 0) {
+                subscription.fail(EconomyException(EconomyFailureReason.NEGATIVE_BALANCES_NOT_SUPPORTED))
+                return
+            }
+            subscription.succeed(BigDecimal.valueOf(value))
+        } catch (e: NumberFormatException) {
+            subscription.fail(EconomyException(TreasuryFailureReasons.INVALID_VALUE, e))
+        }
+    }
+
+    private fun matches(currency: String): Boolean {
+        return if (currency.length == 1) {
+            currency[0] == decimal
+        } else {
+            (currency.equals(symbol, ignoreCase = true)
+                    || currency.equals(displayNameSingular, ignoreCase = true)
+                    || currency.equals(displayNamePlural, ignoreCase = true))
+        }
+    }
+
+    private fun isSeparator(c: Char): Boolean {
+        return c == decimal || c == separator()
+    }
+
+    private fun separator(): Char {
+        val separator: String = ",".trim()
+        return if (separator.isEmpty()) ',' else separator[0]
     }
 
     override fun getStartingBalance(playerID: UUID?): BigDecimal {
