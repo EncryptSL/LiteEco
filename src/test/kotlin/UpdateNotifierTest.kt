8f4ed1e1faf9e4fcc4faf41dd3ef478902bc4f1c
Lucyy_ <conahiromi@gmail.com>
2023-01-15 13:26:21 -0300

    Update UpdateNotifierTest.kt

--- src/test/kotlin/UpdateNotifierTest.kt
+++ src/test/kotlin/UpdateNotifierTest.kt
@@ -6,7 +6,7 @@ class UpdateNotifierTest {
 
     @Test
     fun testVersion() {
-        val update = UpdateNotifier("101934", "1.2.0-SNAPSHOT")
+        val update = UpdateNotifier("101934", "1.2.1-SNAPSHOT")
         val response = update.checkPluginVersion()
         assertEquals("You are using current version !", response)
     }
