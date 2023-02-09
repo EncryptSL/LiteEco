718b914577f0e7781dc612af8c0c6519944c604a
EncryptSL <Rydlos123@gmail.com>
2023-02-09 10:07:39 +0100

    Now numbers are fixed on two maximumFractionDigits

--- src/main/kotlin/encryptsl/cekuj/net/extensions/MoneyFormat.kt
+++ src/main/kotlin/encryptsl/cekuj/net/extensions/MoneyFormat.kt
@@ -6,10 +6,12 @@ import java.util.*
 
 fun Double.moneyFormat(prefix: String, currencyName: String): String {
     val formatter = DecimalFormat("$prefix###,###,##0.00 $currencyName", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
+    formatter.maximumFractionDigits = 2
     return formatter.format(this)
 }
 
 fun Double.moneyFormat(): String {
     val formatter = DecimalFormat("###,###,##0.00", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
+    formatter.maximumFractionDigits = 2
     return formatter.format(this)
 }
