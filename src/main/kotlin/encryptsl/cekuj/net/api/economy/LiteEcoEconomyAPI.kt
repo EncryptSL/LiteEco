ae98edc8ab570d6532925a6a3f3179b678090090
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-21 01:57:07 -0300

    Implemented compact representations for formatting and refactored MoneyFormat

--- src/main/kotlin/encryptsl/cekuj/net/api/economy/LiteEcoEconomyAPI.kt
+++ src/main/kotlin/encryptsl/cekuj/net/api/economy/LiteEcoEconomyAPI.kt
@@ -53,6 +53,6 @@ class LiteEcoEconomyAPI(private val liteEco: LiteEco) : LiteEconomyAPIProvider {
     }
 
     override fun formatting(amount: Double): String {
-        return amount.moneyFormat(liteEco.config.getString("plugin.economy.prefix").toString(), liteEco.config.getString("plugin.economy.name").toString())
+        return amount.moneyFormat(liteEco.config.getString("plugin.economy.prefix").toString(), liteEco.config.getString("plugin.economy.name").toString(), liteEco.config.getString("plugin.economy.compact_display").toBoolean())
     }
 }
--- src/main/kotlin/encryptsl/cekuj/net/extensions/MoneyFormat.kt
+++ src/main/kotlin/encryptsl/cekuj/net/extensions/MoneyFormat.kt
@@ -4,10 +4,18 @@ import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.*
 
-fun Double.moneyFormat(prefix: String, currencyName: String): String {
-    val formatter = DecimalFormat("$prefix###,###,##0.00 $currencyName", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
+fun Double.moneyFormat(prefix: String, currencyName: String, compact: Boolean = false): String {
+    val formatter = DecimalFormat("###,###,##0.00", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
     formatter.maximumFractionDigits = 2
-    return formatter.format(this)
+
+    val compactData = toCompact(this)
+    val number = compactData?.first ?: this
+    var suffix = " $currencyName"
+    if (compact) {
+        suffix = compactData?.second?.toString() + suffix
+    }
+
+    return "$prefix${formatter.format(number)}$suffix"
 }
 
 fun Double.moneyFormat(): String {
@@ -15,15 +23,37 @@ fun Double.moneyFormat(): String {
     formatter.maximumFractionDigits = 2
     return formatter.format(this)
 }
+
+private fun toCompact(number: Double): Pair<Double, Char>? {
+    val units = charArrayOf('K', 'M', 'B', 'T')
+
+    if (number < 1000) {
+        return null
+    }
+
+    var unitIndex = 0
+    var value = number
+    while (value >= 1000 && unitIndex < units.size) {
+        value /= 1000
+        unitIndex++
+    }
+
+    return if (unitIndex > 0) {
+        Pair(value, units[unitIndex - 1])
+    } else {
+        null
+    }
+}
+
 fun String.toValidNumber(): Double? {
-    val units = mapOf('K' to 1000.0, 'M' to 1_000_000.0, 'B' to 1_000_000_000.0, 'T' to 1_000_000_000_000.0)
+    val units = mapOf('K' to 1000.0, 'M' to 1_000_000.0, 'B' to 1_000_000_000.0, 'T' to 1_000_000_000_000.0, 'Q' to 1_000_000_000_000_000.0)
 
     val lastChar = this.lastOrNull()?.uppercaseChar()
     if (lastChar == null || !units.containsKey(lastChar)) {
         return toDoubleOrNull()
     }
     val multiplier = units[lastChar]!!
-    val value = dropLast(1)?.toDoubleOrNull()!!
+    val value = dropLast(1).toDoubleOrNull()!!
 
     return value * multiplier
 }
