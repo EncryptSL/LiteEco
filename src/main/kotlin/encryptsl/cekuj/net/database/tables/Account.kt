b15573143fef56cae4b7699158b49fd7345541ef
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-21 17:32:28 -0300

    Refactored object name Money -> Account

--- src/main/kotlin/encryptsl/cekuj/net/database/tables/Account.kt
+++ src/main/kotlin/encryptsl/cekuj/net/database/tables/Account.kt
@@ -2,7 +2,7 @@ package encryptsl.cekuj.net.database.tables
 
 import org.jetbrains.exposed.sql.Table
 
-object Money : Table() {
+object Account : Table() {
     private val id = integer( "id").autoIncrement()
     val uuid = varchar("uuid", 36)
     val money = double("money")
