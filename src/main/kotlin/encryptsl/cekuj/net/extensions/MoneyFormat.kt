f8b0233d6eeac4a1f7c4e2c5a2dfb291d9ecafbe
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-22 20:50:32 -0300

    Removing trailingZeros & fixed roundingMode

--- src/main/kotlin/encryptsl/cekuj/net/extensions/MoneyFormat.kt
+++ src/main/kotlin/encryptsl/cekuj/net/extensions/MoneyFormat.kt
@@ -1,39 +1,56 @@
 package encryptsl.cekuj.net.extensions
 
+import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.*
 
 
-private fun formatter(): DecimalFormat {
-    val formatter = DecimalFormat("###,###,##0.00", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
-    formatter.maximumFractionDigits = 2
+private fun formatter(fractionDigits: Int = 2, roundingMode: RoundingMode): DecimalFormat {
+    val formatter = DecimalFormat("###,###,##0.000", DecimalFormatSymbols.getInstance(Locale.ENGLISH))
+    formatter.maximumFractionDigits = fractionDigits
+    formatter.roundingMode = roundingMode
     return formatter
 }
 
 private fun formatNumber(number: Double, compact: Boolean): Pair<String, Char?> {
     return if (compact) {
         val (compactNumber, compactChar) = compactNumber(number) ?: (number to null)
-        formatter().format(compactNumber) to compactChar
+        removeTrailingZeros(formatter(roundingMode = RoundingMode.DOWN).format(compactNumber)) to compactChar
     } else {
-        formatter().format(number) to null
+        formatter(roundingMode = RoundingMode.HALF_UP).format(number) to null
     }
 }
 
 fun Double.moneyFormat(prefix: String, currencyName: String, compact: Boolean = false): String {
     val (formattedNumber, compactChar) = formatNumber(this, compact)
-    val suffix = compactChar?.let { " $it $currencyName" } ?: " $currencyName"
+    val suffix = compactChar?.let { "$it $currencyName" } ?: " $currencyName"
 
     return "$prefix$formattedNumber$suffix"
 }
 
 fun Double.moneyFormat(compact: Boolean = false): String {
     val (formattedNumber, compactChar) = formatNumber(this, compact)
-    val suffix = compactChar?.let { " $it" } ?: ""
+    val suffix = compactChar?.let { "$it" } ?: ""
 
     return "$formattedNumber$suffix"
 }
 
+private fun removeTrailingZeros(numberStr: String): String {
+    return if (numberStr.contains('.') && numberStr.endsWith("0")) {
+        var i = numberStr.length - 1
+        while (i >= 0 && numberStr[i] == '0') {
+            i--
+        }
+        if (numberStr[i] == '.') {
+            numberStr.substring(0, i)
+        } else {
+            numberStr.substring(0, i + 1)
+        }
+    } else {
+        numberStr
+    }
+}
 private fun compactNumber(number: Double): Pair<Double, Char>? {
     val units = charArrayOf('K', 'M', 'B', 'T', 'Q')
 
