e999ece3d83932c13c4e007f33e3572d9ed65735
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-30 19:51:15 -0300

    Remove unused extensions

--- src/main/kotlin/encryptsl/cekuj/net/extensions/NumberControl.kt
+++ src/main/kotlin/encryptsl/cekuj/net/extensions/NumberControl.kt
@@ -7,7 +7,3 @@ fun Double.isNegative(): Boolean {
 fun Double.isZero(): Boolean {
     return this == 0.0
 }
-fun String.isNumeric(): Boolean {
-    if (this.isEmpty()) return false
-    return this.toIntOrNull() != null
-}
--- src/main/kotlin/encryptsl/cekuj/net/extensions/TopPosition.kt
+++ src/main/kotlin/encryptsl/cekuj/net/extensions/TopPosition.kt
@@ -1,10 +1,5 @@
 package encryptsl.cekuj.net.extensions
 
-inline fun <K, V, M : Map<out K, V>> M.playerPosition(action: (index: Int, Map.Entry<K, V>) -> Unit) {
-    var index = 1
-    for (item in this) action(index++, item)
-}
-
 inline fun <T, R> Iterable<T>.positionIndexed(transform: (index: Int, T) -> R): List<R> {
     return mapIndexedTo(ArrayList(collectionSizeOrDefault(10)), transform)
 }
