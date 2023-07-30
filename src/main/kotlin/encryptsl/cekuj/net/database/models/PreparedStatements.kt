a0eb56fb573d506103935698cd90ae03e7d1002e
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-30 19:59:13 -0300

    Refactor PreparedStatements

--- src/main/kotlin/encryptsl/cekuj/net/database/models/PreparedStatements.kt
+++ src/main/kotlin/encryptsl/cekuj/net/database/models/PreparedStatements.kt
@@ -4,67 +4,69 @@ import encryptsl.cekuj.net.api.interfaces.DatabaseSQLProvider
 import encryptsl.cekuj.net.database.tables.Account
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.minus
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.notInList
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.plus
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
 class PreparedStatements : DatabaseSQLProvider {
 
     override fun createPlayerAccount(uuid: UUID, money: Double) {
-            transaction { Account.insert {
+        transaction {
+            Account.insertIgnore {
                 it[Account.uuid] = uuid.toString()
                 it[Account.money] = money
-            } }
+            }
+        }
     }
 
     override fun deletePlayerAccount(uuid: UUID) {
-        transaction { Account.deleteWhere { Account.uuid eq uuid.toString() } }
-    }
-
-    override fun getExistPlayerAccount(uuid: UUID): Boolean
-        = transaction { !Account.select(Account.uuid eq uuid.toString() ).empty() }
-
-    override fun getTopBalance(top: Int): MutableMap<String, Double> {
-        val hashMap: HashMap<String, Double> = HashMap()
         transaction {
-            Account.selectAll().limit(top).forEach { a ->
-                hashMap[a[Account.uuid]] = a[Account.money]
-            }
+            Account.deleteWhere { Account.uuid eq uuid.toString() }
         }
-
-        return hashMap
     }
 
-    override fun getTopBalance(): MutableMap<String, Double> {
-        val hashMap: HashMap<String, Double> = HashMap()
-        transaction {
-            Account.selectAll().forEach { a ->
-                hashMap[a[Account.uuid]] = a[Account.money]
-            }
-        }
-
-        return hashMap
+    override fun getExistPlayerAccount(uuid: UUID): Boolean = transaction {
+        !Account.select(Account.uuid eq uuid.toString()).empty()
     }
 
-    override fun getBalance(uuid: UUID): Double
-        = transaction { Account.select(Account.uuid eq uuid.toString()).first()[Account.money] }
+    override fun getTopBalance(top: Int): MutableMap<String, Double> = transaction {
+        Account.selectAll().limit(top).associate {
+            it[Account.uuid] to it[Account.money]
+        }.toMutableMap()
+    }
+
+    override fun getTopBalance(): MutableMap<String, Double> = transaction {
+        Account.selectAll().associate {
+            it[Account.uuid] to it[Account.money]
+        }.toMutableMap()
+    }
+
+    override fun getBalance(uuid: UUID): Double = transaction {
+        Account.select(Account.uuid eq uuid.toString()).first()[Account.money]
+    }
 
     override fun depositMoney(uuid: UUID, money: Double) {
-        transaction { Account.update ({ Account.uuid eq uuid.toString() }) {
-            it[Account.money] = money
-        } }
+        transaction {
+            Account.update({ Account.uuid eq uuid.toString() }) {
+                it[Account.money] = Account.money plus money
+            }
+        }
     }
-
     override fun withdrawMoney(uuid: UUID, money: Double) {
-        transaction { Account.update ({ Account.uuid eq uuid.toString() }) {
-            it[Account.money] = money
-        } }
+        transaction {
+            Account.update({ Account.uuid eq uuid.toString() }) {
+                it[Account.money] = Account.money minus money
+            }
+        }
     }
-
     override fun setMoney(uuid: UUID, money: Double) {
-        transaction { Account.update ({ Account.uuid eq uuid.toString() }) {
-            it[Account.money] = money
-        } }
+        transaction {
+            Account.update({ Account.uuid eq uuid.toString() }) {
+                it[Account.money] = money
+            }
+        }
     }
 
     override fun purgeAccounts() {
