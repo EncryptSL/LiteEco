71e1c3e9c0a8d941c0a51ef2685985a84b61d2a2
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2023-07-31 01:07:38 -0300

    Update ConfigAPIProvider.kt

--- src/main/kotlin/encryptsl/cekuj/net/api/interfaces/ConfigAPIProvider.kt
+++ src/main/kotlin/encryptsl/cekuj/net/api/interfaces/ConfigAPIProvider.kt
@@ -3,13 +3,13 @@ package encryptsl.cekuj.net.api.interfaces
 import encryptsl.cekuj.net.api.ConfigAPI
 
 interface ConfigAPIProvider {
-    fun create(configName: String): ConfigAPI
+    fun create(fileName: String): ConfigAPI
 
     /**
      * Create config.yml
-     * @param configName - Config name example config.yml
+     * @param fileName - File name example config.yml
      * @param version - Version of revision of config need changed.
      * @return ConfigLoaderAPI
      */
-    fun createConfig(configName: String, version: String): ConfigAPI
+    fun createConfig(fileName: String, version: String): ConfigAPI
 }
