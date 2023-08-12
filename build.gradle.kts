3730c0155742b4b5d97f68462ebea706913506ab
EncryptSL <Rydlos123@gmail.com>
2023-08-12 06:11:09 +0200

    Updating of kotlin Updating of exposed database framework.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.20" apply true
+    kotlin("jvm") version "1.9.0" apply true
     id("io.papermc.paperweight.userdev") version "1.5.5"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
@@ -33,15 +33,15 @@ kotlin {
 
 dependencies {
     paperweight.paperDevBundle(providers.gradleProperty("server_version").get())
-    compileOnly(kotlin("stdlib", "1.8.20"))
+    compileOnly(kotlin("stdlib", "1.9.0"))
     compileOnly("me.lokka30:treasury-api:1.2.1")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude("org.bukkit", "bukkit")
     }
     compileOnly("com.zaxxer:HikariCP:5.0.1")
     compileOnly("me.clip:placeholderapi:2.11.3")
-    compileOnly("org.jetbrains.exposed:exposed-core:0.40.1")
-    compileOnly("org.jetbrains.exposed:exposed-jdbc:0.40.1")
+    compileOnly("org.jetbrains.exposed:exposed-core:0.42.0")
+    compileOnly("org.jetbrains.exposed:exposed-jdbc:0.42.0")
     compileOnly("com.squareup.okhttp3:okhttp:4.10.0")
 
     implementation("org.bstats:bstats-bukkit:3.0.0")
@@ -50,16 +50,16 @@ dependencies {
 
     testImplementation(kotlin("test"))
     testImplementation("com.zaxxer:HikariCP:5.0.1")
-    testImplementation("org.xerial:sqlite-jdbc:3.40.0.0")
-    testImplementation("org.jetbrains.exposed:exposed-core:0.40.1")
-    testImplementation("org.jetbrains.exposed:exposed-jdbc:0.40.1")
+    testImplementation("org.xerial:sqlite-jdbc:3.42.0.0")
+    testImplementation("org.jetbrains.exposed:exposed-core:0.42.0")
+    testImplementation("org.jetbrains.exposed:exposed-jdbc:0.42.0")
     testImplementation("com.squareup.okhttp3:okhttp:4.10.0")
 }
 
 tasks {
 
     build {
-        dependsOn(shadowJar)
+        dependsOn(reobfJar)
     }
 
     test {
