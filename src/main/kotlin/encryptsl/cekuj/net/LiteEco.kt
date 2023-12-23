cb1f10049d8003515716a0161aae5caaa16ed55c
EncryptSL <Rydlos123@gmail.com>
2023-12-24 00:57:31 +0100

    Updated config version.

--- src/main/kotlin/encryptsl/cekuj/net/LiteEco.kt
+++ src/main/kotlin/encryptsl/cekuj/net/LiteEco.kt
@@ -33,7 +33,7 @@ import kotlin.system.measureTimeMillis
 
 class LiteEco : JavaPlugin() {
     companion object {
-        const val CONFIG_VERSION = "1.2.1"
+        const val CONFIG_VERSION = "1.2.2"
         const val LANG_VERSION = "2.0.1"
         const val PAPI_VERSION = "2.0.5"
     }
