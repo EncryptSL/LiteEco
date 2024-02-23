62265f97d6f7c562461ff4f28f952abfb22abbb1
EncryptSL <Rydlos123@gmail.com>
2024-02-23 03:15:29 +0100

    Table name moved into Table(blabla..)

--- src/main/kotlin/encryptsl/cekuj/net/database/tables/Account.kt
+++ src/main/kotlin/encryptsl/cekuj/net/database/tables/Account.kt
@@ -2,14 +2,11 @@ package encryptsl.cekuj.net.database.tables
 
 import org.jetbrains.exposed.sql.Table
 
-object Account : Table() {
+object Account : Table("lite_eco") {
     private val id = integer( "id").autoIncrement()
     val username = varchar("username", 36)
     val uuid = varchar("uuid", 36)
     val money = double("money")
 
     override val primaryKey: PrimaryKey = PrimaryKey(id)
-
-    override val tableName: String
-        get() = "lite_eco"
 }
