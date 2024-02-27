e4e16922e8e191ac4e3b8b49aa11b083741b12bc
EncryptSL <Rydlos123@gmail.com>
2024-02-27 23:19:41 +0100

    Improvements of UpdateNotifier, possible fix for catch TimeoutException.

--- src/main/kotlin/encryptsl/cekuj/net/api/UpdateNotifier.kt
+++ src/main/kotlin/encryptsl/cekuj/net/api/UpdateNotifier.kt
@@ -1,42 +1,43 @@
 package encryptsl.cekuj.net.api
 
 import com.google.gson.Gson
+import encryptsl.cekuj.net.LiteEco
+import okhttp3.Call
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.net.MalformedURLException
-import java.net.URL
+import okhttp3.Response
 
-class UpdateNotifier(private val id: String, private val pluginVersion: String) {
+
+class UpdateNotifier(private val liteEco: LiteEco, private val id: String, private val pluginVersion: String) {
 
     private val okHttpClient = OkHttpClient()
     private val requestClient = Request.Builder()
 
-    private fun makeCheckRequest(): String {
-        val request = requestClient.url(URL(String.format("https://api.spiget.org/v2/resources/%s/versions/latest", id))).build()
-        okHttpClient.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) {
-                response.close()
-                throw MalformedURLException("Request error > ${response.code}")
+    fun makeUpdateCheck() {
+        try {
+            val request: Request = requestClient.url(String.format("https://api.spiget.org/v2/resources/%s/versions/latest", id)).build()
+            val call: Call = okHttpClient.newCall(request)
+            call.execute().use { r ->
+                if (!r.isSuccessful) { r.close() }
+                liteEco.logger.info(logUpdate(r))
             }
-
-            return response.body!!.string()
+        } catch (e : Exception) {
+            liteEco.logger.severe(e.message ?: e.localizedMessage)
         }
     }
 
-    private fun getVersion(): String {
-        val data = makeCheckRequest()
-        val gson = Gson()
-        return gson.fromJson(data, Version::class.java).name
-    }
+    private fun logUpdate(response: Response): String {
+        if (response.code != 200)
+            throw Exception("Excepted http code is 200 but returned is ${response.code}")
 
-    fun checkPluginVersion(): String {
-        return when(val latestVersion = getVersion()) {
+        return when(val latestVersion = Gson().fromJson(response.body?.string(), Version::class.java).name) {
             latestVersion -> "You are using current version !"
             else -> {
                 "Please download update of plugin LiteEco your version: $pluginVersion > Updated version: $latestVersion"
             }
         }
     }
+
 }
 
 data class Version(
