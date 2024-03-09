b347bb2088691dc859a4c8c73f1fcb8bc5298c29
EncryptSL <Rydlos123@gmail.com>
2024-03-09 02:59:17 +0100

    Updated libraries...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.22" apply true
+    kotlin("jvm") version "1.9.23" apply true
     id("io.papermc.paperweight.userdev") version "1.5.10"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("maven-publish")
@@ -31,26 +31,26 @@ kotlin {
 
 dependencies {
     paperweight.paperDevBundle(providers.gradleProperty("server_version").get())
-    compileOnly(kotlin("stdlib", "1.9.22"))
+    compileOnly(kotlin("stdlib", "1.9.23"))
     compileOnly("me.lokka30:treasury-api:1.2.1")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude("org.bukkit", "bukkit")
     }
     compileOnly("com.zaxxer:HikariCP:5.1.0")
     compileOnly("me.clip:placeholderapi:2.11.5")
-    compileOnly("org.jetbrains.exposed:exposed-core:0.46.0")
-    compileOnly("org.jetbrains.exposed:exposed-jdbc:0.46.0")
+    compileOnly("org.jetbrains.exposed:exposed-core:0.48.0")
+    compileOnly("org.jetbrains.exposed:exposed-jdbc:0.48.0")
     compileOnly("com.squareup.okhttp3:okhttp:4.12.0")
 
     implementation("org.bstats:bstats-bukkit:3.0.1")
     implementation("org.incendo:cloud-paper:2.0.0-beta.2")
     implementation("org.incendo:cloud-annotations:2.0.0-beta.2")
 
-    testImplementation(kotlin("test", "1.9.22"))
+    testImplementation(kotlin("test", "1.9.23"))
     testImplementation("com.zaxxer:HikariCP:5.1.0")
     testImplementation("org.xerial:sqlite-jdbc:3.42.0.0")
-    testImplementation("org.jetbrains.exposed:exposed-core:0.47.0")
-    testImplementation("org.jetbrains.exposed:exposed-jdbc:0.47.0")
+    testImplementation("org.jetbrains.exposed:exposed-core:0.48.0")
+    testImplementation("org.jetbrains.exposed:exposed-jdbc:0.48.0")
 }
 
 tasks {
