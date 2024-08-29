4185e3b2469f3a2fb48e35a604239852a6401579
EncryptSL <Rydlos123@gmail.com>
2024-08-29 04:14:48 +0200

    This old API was changed to DeprecatedLiteEcoEconomyImpl and added to abstract layer of api.
    ModernLiteEcoEconomyImpl is compatible with this old because is extended by this abstract.

--- src/main/kotlin/com/github/encryptsl/lite/eco/api/economy/DeprecatedLiteEcoEconomyImpl.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/api/economy/DeprecatedLiteEcoEconomyImpl.kt
@@ -3,40 +3,32 @@ package com.github.encryptsl.lite.eco.api.economy
 import com.github.encryptsl.lite.eco.LiteEco
 import com.github.encryptsl.lite.eco.api.PlayerAccount
 import com.github.encryptsl.lite.eco.api.interfaces.LiteEconomyAPI
-import com.github.encryptsl.lite.eco.api.objects.ModernText
 import com.github.encryptsl.lite.eco.common.database.entity.User
-import com.github.encryptsl.lite.eco.common.database.models.DatabaseEcoModel
-import com.github.encryptsl.lite.eco.common.extensions.compactFormat
-import com.github.encryptsl.lite.eco.common.extensions.moneyFormat
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
-import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 import java.math.BigDecimal
-import java.util.*
 import java.util.concurrent.CompletableFuture
 
-class LiteEcoEconomyImpl : LiteEconomyAPI {
-
-    private val databaseEcoModel: DatabaseEcoModel by lazy { DatabaseEcoModel() }
+abstract class DeprecatedLiteEcoEconomyImpl : LiteEconomyAPI {
 
     override fun createAccount(player: OfflinePlayer, currency: String, startAmount: BigDecimal): Boolean {
         return getUserByUUID(player, currency).thenApply {
+            LiteEco.instance.databaseEcoModel.updatePlayerName(player.uniqueId, player.name.toString(), currency)
             return@thenApply false
         }.exceptionally {
-            databaseEcoModel.createPlayerAccount(player.name.toString(), player.uniqueId, currency, startAmount)
+            LiteEco.instance.databaseEcoModel.createPlayerAccount(player.name.toString(), player.uniqueId, currency, startAmount)
             return@exceptionally true
         }.join()
     }
 
     override fun cacheAccount(player: OfflinePlayer, currency: String, amount: BigDecimal) {
-        getUserByUUID(player, currency).thenAccept { PlayerAccount.cacheAccount(player.uniqueId, currency, amount) }
+        PlayerAccount.cacheAccount(player.uniqueId, currency, amount)
     }
 
     override fun deleteAccount(player: OfflinePlayer, currency: String): Boolean {
         return getUserByUUID(player, currency).thenApply {
             PlayerAccount.clearFromCache(player.uniqueId)
-            databaseEcoModel.deletePlayerAccount(player.uniqueId, currency)
+            LiteEco.instance.databaseEcoModel.deletePlayerAccount(player.uniqueId, currency)
             return@thenApply true
         }.exceptionally {
             return@exceptionally false
@@ -44,7 +36,7 @@ class LiteEcoEconomyImpl : LiteEconomyAPI {
     }
 
     override fun hasAccount(player: OfflinePlayer, currency: String): CompletableFuture<Boolean> {
-        return databaseEcoModel.getExistPlayerAccount(player.uniqueId, currency)
+        return LiteEco.instance.databaseEcoModel.getExistPlayerAccount(player.uniqueId, currency)
     }
 
     override fun has(player: OfflinePlayer, currency: String, amount: BigDecimal): Boolean {
@@ -54,22 +46,10 @@ class LiteEcoEconomyImpl : LiteEconomyAPI {
     override fun getUserByUUID(player: OfflinePlayer, currency: String): CompletableFuture<User> {
         val future = CompletableFuture<User>()
 
-        if (PlayerAccount.isPlayerOnline(player.uniqueId) || PlayerAccount.isAccountCached(player.uniqueId)) {
+        if (PlayerAccount.isPlayerOnline(player.uniqueId) || PlayerAccount.isAccountCached(player.uniqueId, currency)) {
             future.completeAsync { User(player.name.toString(), player.uniqueId, PlayerAccount.getBalance(player.uniqueId, currency)) }
         } else {
-            return databaseEcoModel.getUserByUUID(player.uniqueId, currency)
-        }
-
-        return future
-    }
-
-    override fun getUserByUUID(uuid: UUID, currency: String): CompletableFuture<User> {
-        val future = CompletableFuture<User>()
-
-        if (PlayerAccount.isPlayerOnline(uuid) || PlayerAccount.isAccountCached(uuid)) {
-            future.completeAsync { User(uuid.toString(), uuid, PlayerAccount.getBalance(uuid, currency)) }
-        } else {
-            return databaseEcoModel.getUserByUUID(uuid, currency)
+            return LiteEco.instance.databaseEcoModel.getUserByUUID(player.uniqueId, currency)
         }
 
         return future
@@ -77,10 +57,6 @@ class LiteEcoEconomyImpl : LiteEconomyAPI {
 
     override fun getBalance(player: OfflinePlayer, currency: String): BigDecimal {
         return getUserByUUID(player, currency).join().money
-    }
-
-    override fun getBalance(uuid: UUID, currency: String): BigDecimal {
-        return getUserByUUID(uuid, currency).join().money
     }
 
     override fun getCheckBalanceLimit(amount: BigDecimal, currency: String): Boolean {
@@ -95,7 +71,7 @@ class LiteEcoEconomyImpl : LiteEconomyAPI {
         if (PlayerAccount.isPlayerOnline(player.uniqueId)) {
             cacheAccount(player, currency, getBalance(player, currency).plus(amount))
         } else {
-            databaseEcoModel.depositMoney(player.uniqueId, currency, amount)
+            LiteEco.instance.databaseEcoModel.depositMoney(player.uniqueId, currency, amount)
         }
     }
 
@@ -103,7 +79,7 @@ class LiteEcoEconomyImpl : LiteEconomyAPI {
         if (PlayerAccount.isPlayerOnline(player.uniqueId)) {
             cacheAccount(player, currency, getBalance(player, currency).minus(amount))
         } else {
-            databaseEcoModel.withdrawMoney(player.uniqueId, currency, amount)
+            LiteEco.instance.databaseEcoModel.withdrawMoney(player.uniqueId, currency, amount)
         }
     }
 
@@ -116,7 +92,7 @@ class LiteEcoEconomyImpl : LiteEconomyAPI {
         if (PlayerAccount.isPlayerOnline(player.uniqueId)) {
             cacheAccount(player, currency, amount)
         } else {
-            databaseEcoModel.setMoney(player.uniqueId, currency, amount)
+            LiteEco.instance.databaseEcoModel.setMoney(player.uniqueId, currency, amount)
         }
     }
 
@@ -125,48 +101,5 @@ class LiteEcoEconomyImpl : LiteEconomyAPI {
             return PlayerAccount.syncAccount(offlinePlayer.uniqueId, currency, LiteEco.instance.currencyImpl.getCurrencyLimit(currency))
 
         PlayerAccount.syncAccount(offlinePlayer.uniqueId)
-    }
-
-    override fun syncAccounts() {
-        PlayerAccount.syncAccounts()
-    }
-
-    override fun getTopBalance(currency: String): Map<String, BigDecimal> {
-        val database = databaseEcoModel.getTopBalance(currency)
-            .mapValues { e -> if (PlayerAccount.isAccountCached(e.value.uuid)) PlayerAccount.getBalance(e.value.uuid, currency) else e.value.money}
-            .filterKeys { e -> !e.equals("NULL", true)  }.toList()
-
-        return database.sortedByDescending { (_, e) -> e }.toMap()
-    }
-
-
-    override fun getUUIDNameMap(currency: String): MutableMap<UUID, String> {
-        return databaseEcoModel.getUUIDNameMap(currency)
-    }
-
-    override fun compacted(amount: BigDecimal): String {
-        return amount.compactFormat(
-            LiteEco.instance.config.getString("formatting.currency_pattern").toString(),
-            LiteEco.instance.config.getString("formatting.compacted_pattern").toString(),
-            LiteEco.instance.config.getString("formatting.currency_locale").toString()
-        )
-    }
-
-    override fun formatted(amount: BigDecimal): String {
-        return amount.moneyFormat(
-            LiteEco.instance.config.getString("formatting.currency_pattern").toString(),
-            LiteEco.instance.config.getString("formatting.currency_locale").toString()
-        )
-    }
-
-    override fun fullFormatting(amount: BigDecimal, currency: String): String {
-
-        val value = when(LiteEco.instance.currencyImpl.isCurrencyDisplayCompactEnabled(currency)) {
-            true -> compacted(amount)
-            false -> formatted(amount)
-        }
-        return LiteEco.instance.locale.plainTextTranslation(ModernText.miniModernText(
-            LiteEco.instance.currencyImpl.getCurrencyFormat(currency), Placeholder.parsed("money", value))
-        )
     }
 }
