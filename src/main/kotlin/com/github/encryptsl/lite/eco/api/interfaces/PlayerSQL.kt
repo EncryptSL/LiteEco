0237b17fba8da4cfa587033e2d43e5cab80748c8
EncryptSL <rydlos123@gmail.com>
2025-05-27 17:13:21 +0200

    Removed CompletableFuture

--- src/main/kotlin/com/github/encryptsl/lite/eco/api/interfaces/PlayerSQL.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/api/interfaces/PlayerSQL.kt
@@ -3,18 +3,17 @@ package com.github.encryptsl.lite.eco.api.interfaces
 import com.github.encryptsl.lite.eco.common.database.entity.User
 import java.math.BigDecimal
 import java.util.*
-import java.util.concurrent.CompletableFuture
 
 interface PlayerSQL {
     fun createPlayerAccount(username: String, uuid: UUID, currency: String, money: BigDecimal)
     fun updatePlayerName(uuid: UUID, username: String, currency: String)
-    fun getUserByUUID(uuid: UUID, currency: String): CompletableFuture<Optional<User>>
+    fun getUserByUUID(uuid: UUID, currency: String): User?
     fun getBalance(uuid: UUID, currency: String): BigDecimal
     fun deletePlayerAccount(uuid: UUID, currency: String)
-    fun getExistPlayerAccount(uuid: UUID, currency: String): CompletableFuture<Boolean>
+    fun getExistPlayerAccount(uuid: UUID, currency: String): Boolean
     fun getTopBalance(currency: String): MutableMap<String, User>
     fun getUUIDNameMap(currency: String): MutableMap<UUID, String>
-    fun getPlayersIds(currency: String): CompletableFuture<MutableCollection<UUID>>
+    fun getPlayersIds(currency: String): MutableCollection<UUID>
     fun depositMoney(uuid: UUID, currency: String, money: BigDecimal)
     fun withdrawMoney(uuid: UUID, currency: String, money: BigDecimal)
     fun setMoney(uuid: UUID, currency: String, money: BigDecimal)
