8380bc86305fa8d3327cf560c8adf285d8ebf6d4
EncryptSL <rydlos123@gmail.com>
2025-05-29 02:31:29 +0200

    Fixed comments and grammar.

--- src/main/kotlin/com/github/encryptsl/lite/eco/api/interfaces/LiteEconomyAPI.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/api/interfaces/LiteEconomyAPI.kt
@@ -2,180 +2,194 @@ package com.github.encryptsl.lite.eco.api.interfaces
 
 import com.github.encryptsl.lite.eco.common.database.entity.User
 import org.bukkit.OfflinePlayer
-import org.bukkit.entity.Player
 import java.math.BigDecimal
 import java.util.*
 
 interface LiteEconomyAPI {
     /**
-     * Create player account to database
-     * @param player is OfflinePlayer
-     * @param currency name of currency.
-     * @param startAmount an amount added to player when accounts is created.
-     * @return Boolean
-     * @see Boolean
-     * @see OfflinePlayer
+     * Creates a player account in the database.
+     *
+     * @param player The offline player instance.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param startAmount The initial amount to be added to the account.
+     * @return `true` if the account was successfully created, otherwise `false`.
      */
     fun createAccount(player: OfflinePlayer, currency: String = "dollars", startAmount: BigDecimal): Boolean
 
     /**
-     * Cache player account during login
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @param amount is value of player account from database.
-     * @return Boolean
-     * @see OfflinePlayer
-     * @see com.github.encryptsl.lite.eco.common.database.models.DatabaseEcoModel.getUserByUUID(uuid)
+     * Creates a player account in the database.
+     *
+     * @param uuid The player's UUID.
+     * @param username The player's username.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param startAmount The initial amount to be added to the account.
+     * @return `true` if the account was successfully created, otherwise `false`.
+     */
+    fun createAccount(uuid: UUID, username: String, currency: String = "dollars", startAmount: BigDecimal): Boolean
+
+    /**
+     * Caches a player's account during login.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param amount The account balance from the database.
      */
     fun cacheAccount(uuid: UUID, currency: String = "dollars", amount: BigDecimal)
 
     /**
-     * Delete player account from database
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @return Boolean
-     * @see OfflinePlayer
+     * Deletes a player's account from the database.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @return `true` if the account was successfully deleted, otherwise `false`.
      */
-    fun deleteAccount(uuid: UUID, currency: String = "dollars"): Boolean
+    fun deleteAccount(uuid: UUID, currency: String = "dollars" ): Boolean
 
     /**
-     * Boolean for check if player have account in database
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @return Boolean
-     * @see UUID
+     * Checks if a player has an account in the database.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @return `true` if the account exists, otherwise `false`.
      */
-    fun hasAccount(uuid: UUID, currency: String = "dollars"): Boolean
+    fun hasAccount(uuid: UUID, currency: String = "dollars" ): Boolean
 
     /**
-     * Boolean for check if player have enough money
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @return Boolean
-     * @see UUID
+     * Checks if a player has at least a certain amount of money.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param amount The amount to check against.
+     * @return `true` if the player has at least the specified amount, otherwise `false`.
      */
     fun has(uuid: UUID, currency: String = "dollars", amount: BigDecimal): Boolean
 
     /**
-     * Get user account
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @return User
-     * @see OfflinePlayer
+     * Retrieves a player's account by UUID.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @return An [Optional] containing the [User], if found.
      */
-    fun getUserByUUID(uuid: UUID, currency: String = "dollars"): Optional<User>
+    fun getUserByUUID(uuid: UUID, currency: String = "dollars" ): Optional<User>
 
     /**
-     * Get balance of player account
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @return Double
-     * @see OfflinePlayer
+     * Retrieves the balance of a player's account.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @return The account balance.
      */
-    fun getBalance(uuid: UUID, currency: String = "dollars"): BigDecimal
+    fun getBalance(uuid: UUID, currency: String = "dollars" ): BigDecimal
 
     /**
-     * Get check limit of player balance.
-     * @param amount is BigDecimal
-     * @param currency name of currency
-     * @return Boolean
-     * @see OfflinePlayer
+     * Checks whether the specified amount exceeds the allowed balance limit.
+     *
+     * @param amount The amount to check.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @return `true` if the amount is within the limit, otherwise `false`.
      */
-    fun getCheckBalanceLimit(amount: BigDecimal, currency: String = "dollars"): Boolean
+    fun getCheckBalanceLimit(amount: BigDecimal, currency: String = "dollars" ): Boolean
 
     /**
-     * Get check limit of player balance.
-     * @param uuid is UUID
-     * @param currentBalance is Current Balance of Player
-     * @param currency name of currency
-     * @return Boolean
-     * @see UUID
+     * Checks whether the specified new balance exceeds the allowed balance limit for a player.
+     *
+     * @param uuid The player's UUID.
+     * @param currentBalance The player's current balance.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param amount The new amount to add.
+     * @return `true` if the resulting balance is within the limit, otherwise `false`.
      */
     fun getCheckBalanceLimit(uuid: UUID, currentBalance: BigDecimal, currency: String = "dollars", amount: BigDecimal): Boolean
 
     /**
-     * Deposit money to player account
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @param amount is amount added to player account
-     * @see UUID
+     * Deposits money into a player's account.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param amount The amount to deposit.
      */
-     fun depositMoney(uuid: UUID, currency: String = "dollars", amount: BigDecimal)
+    fun depositMoney(uuid: UUID, currency: String = "dollars", amount: BigDecimal)
 
     /**
-     * Withdraw money from player account
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @param amount is amount removed from player account
-     * @see UUID
+     * Withdraws money from a player's account.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param amount The amount to withdraw.
      */
     fun withDrawMoney(uuid: UUID, currency: String = "dollars", amount: BigDecimal)
 
     /**
-     * Transfer of money from player account to another player account.
-     * @param fromUUID is sender
-     * @param target is receiver.
-     * @param currency name of currency
-     * @see Player
-     * @see UUID
+     * Transfers money from one player to another.
+     *
+     * @param fromUUID The UUID of the sender.
+     * @param target The UUID of the receiver.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param amount The amount to transfer.
      */
     fun transfer(fromUUID: UUID, target: UUID, currency: String = "dollars", amount: BigDecimal)
 
     /**
-     * Set fixed money to player account
-     * @param uuid is UUID
-     * @param currency name of currency
-     * @param amount is amount fixed value
-     * @see UUID
+     * Sets a fixed amount of money for a player's account.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @param amount The amount to set.
      */
     fun setMoney(uuid: UUID, currency: String = "dollars", amount: BigDecimal)
 
     /**
-     * Synchronize cache with database
-     * @param uuid is UUID
-     * @see UUID
+     * Synchronizes a player's cached account with the database.
+     *
+     * @param uuid The player's UUID.
+     * @param currency The name of the currency. Defaults to "dollars".
      */
     fun syncAccount(uuid: UUID, currency: String = "dollars")
 
     /**
-     * Synchronize all saved data in cache with database
+     * Synchronizes all cached accounts with the database.
      */
     fun syncAccounts()
 
     /**
-     * Get top player accounts
-     * @param currency name of currency
-     * @return MutableMap
+     * Retrieves the top player accounts by balance.
+     *
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @return A map of usernames to their balances.
      */
-    fun getTopBalance(currency: String = "dollars"): Map<String, BigDecimal>
+    fun getTopBalance(currency: String = "dollars" ): Map<String, BigDecimal>
 
     /**
-     * Get top player accounts
-     * @param currency name of currency
-     * @return MutableMap
+     * Retrieves a map of UUIDs to player names.
+     *
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @return A mutable map of UUIDs to player names.
      */
-    fun getUUIDNameMap(currency: String = "dollars"): MutableMap<UUID, String>
+    fun getUUIDNameMap(currency: String = "dollars" ): MutableMap<UUID, String>
 
     /**
-     * Compacted money value
-     * @param amount is only formatted to a compacted value
-     * @return String
+     * Formats a value to a compact money representation.
+     *
+     * @param amount The amount to format.
+     * @return A compacted string representation.
      */
     fun compacted(amount: BigDecimal): String
 
     /**
-     * Formatted money value
-     * @param amount is formatted to readable value
-     * @return String
+     * Formats a value to a human-readable money representation.
+     *
+     * @param amount The amount to format.
+     * @return A formatted string representation.
      */
     fun formatted(amount: BigDecimal): String
 
     /**
-     * Formatting currency value
-     * @param amount is formatted to readable value with currency prefix and name
-     * @param currency name of currency
-     * @return String
+     * Formats a value to a full string representation including the currency.
+     *
+     * @param amount The amount to format.
+     * @param currency The name of the currency. Defaults to "dollars".
+     * @return A fully formatted currency string.
      */
-    fun fullFormatting(amount: BigDecimal, currency: String = "dollars"): String
-
+    fun fullFormatting(amount: BigDecimal, currency: String = "dollars" ): String
 }
