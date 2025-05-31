457068c64d96ad1ed4d9ed9655b7b2e9378e2438
EncryptSL <rydlos123@gmail.com>
2025-05-31 12:51:09 +0200

    Improvement in sql logger added full sql without placeholders for better debug

--- src/main/kotlin/com/github/encryptsl/lite/eco/common/database/SqlPluginLogger.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/common/database/SqlPluginLogger.kt
@@ -5,11 +5,12 @@ import com.github.encryptsl.lite.eco.api.objects.ModernText
 import org.jetbrains.exposed.sql.SqlLogger
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.statements.StatementContext
+import org.jetbrains.exposed.sql.statements.expandArgs
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 
 class SqlPluginLogger : SqlLogger {
     override fun log(context: StatementContext, transaction: Transaction) {
         if (!LiteEco.instance.config.getBoolean("database.sql-plugin-logger")) return
-        LiteEco.instance.componentLogger.info(ModernText.miniModernText("<gold>${context.sql(TransactionManager.current())}"))
+        LiteEco.instance.componentLogger.info(ModernText.miniModernText("<gold>${context.expandArgs(TransactionManager.current())}"))
     }
 }
