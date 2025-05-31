c5d4f09c25d5a7714035c0665a3e6a2d1d6f018f
EncryptSL <rydlos123@gmail.com>
2025-05-31 12:51:45 +0200

    Changed table to LongIdTable now is not needed self implemented primaryKey and id

--- src/main/kotlin/com/github/encryptsl/lite/eco/common/database/tables/Account.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/common/database/tables/Account.kt
@@ -1,12 +1,9 @@
 package com.github.encryptsl.lite.eco.common.database.tables
 
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.dao.id.LongIdTable
 
-class Account(currency: String) : Table("lite_eco_$currency") {
-    private val id = integer("id").autoIncrement()
+class Account(currency: String) : LongIdTable("lite_eco_$currency") {
     val username = varchar("username", 36)
     val uuid = uuid("uuid")
     val money = decimal("money", 18, 9)
-
-    override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
