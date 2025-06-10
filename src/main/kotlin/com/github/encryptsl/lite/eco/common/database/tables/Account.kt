ce67e1755ebb471141c2ea93c6407f1a45723c56
EncryptSL <rydlos123@gmail.com>
2025-06-10 14:47:44 +0200

    Added support for bigger integers.

--- src/main/kotlin/com/github/encryptsl/lite/eco/common/database/tables/Account.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/common/database/tables/Account.kt
@@ -5,5 +5,5 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 class Account(currency: String) : LongIdTable("lite_eco_$currency") {
     val username = varchar("username", 36)
     val uuid = uuid("uuid").uniqueIndex("idx_${currency}_uuid")
-    val money = decimal("money", 18, 9)
+    val money = decimal("money", 30, 2)
 }
