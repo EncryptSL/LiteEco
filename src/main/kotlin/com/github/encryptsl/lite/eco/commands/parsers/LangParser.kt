b575bb566839366b4c7a05b884df0d09a6ae1aba
EncryptSL <rydlos123@gmail.com>
2025-06-19 06:51:29 +0200

    Improvement of LangParser

--- src/main/kotlin/com/github/encryptsl/lite/eco/commands/parsers/LangParser.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/commands/parsers/LangParser.kt
@@ -1,31 +1,31 @@
 package com.github.encryptsl.lite.eco.commands.parsers
 
 import com.github.encryptsl.lite.eco.LiteEco
-import com.github.encryptsl.lite.eco.common.config.Locales
 import org.incendo.cloud.context.CommandContext
 import org.incendo.cloud.context.CommandInput
 import org.incendo.cloud.paper.util.sender.Source
 import org.incendo.cloud.parser.ArgumentParseResult
 import org.incendo.cloud.parser.ArgumentParser
-import org.incendo.cloud.suggestion.Suggestion
 import org.incendo.cloud.suggestion.SuggestionProvider
 
-class LangParser : ArgumentParser<Source, Locales.LangKey> {
+class LangParser(
+    private val liteEco: LiteEco
+) : ArgumentParser<Source, String> {
     override fun parse(
         commandContext: CommandContext<Source>,
         commandInput: CommandInput
-    ): ArgumentParseResult<Locales.LangKey> {
+    ): ArgumentParseResult<String> {
         val input = commandInput.readString()
 
-        try {
-            return ArgumentParseResult.success(Locales.LangKey.valueOf(input))
-        } catch (_ : IllegalArgumentException) {
-            val message = LiteEco.instance.locale.getMessage("messages.parser.error.language_not_exist")
+        if (liteEco.locale.isLocaleAvailable(input)) {
+            return ArgumentParseResult.success(input)
+        } else {
+            val message = liteEco.locale.getMessage("messages.parser.error.language_not_exist")
             return ArgumentParseResult.failure(Exception(String.format(message, input)))
         }
     }
 
     override fun suggestionProvider(): SuggestionProvider<Source> {
-        return SuggestionProvider.suggesting(Locales.LangKey.entries.map { Suggestion.suggestion(it.name) })
+        return SuggestionProvider.suggestingStrings(liteEco.locale.availableLocales())
     }
 }
