fb83909cba8c0a2938a5afd0625cf2177fd6620a
EncryptSL <rydlos123@gmail.com>
2025-06-24 09:13:43 +0200

    Changed economyOperations to typeLogger

--- src/main/kotlin/com/github/encryptsl/lite/eco/api/interfaces/TransactionLogger.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/api/interfaces/TransactionLogger.kt
@@ -1,11 +1,11 @@
 package com.github.encryptsl.lite.eco.api.interfaces
 
-import com.github.encryptsl.lite.eco.api.economy.EconomyOperations
+import com.github.encryptsl.lite.eco.api.enums.TypeLogger
 import com.github.encryptsl.lite.eco.common.database.entity.EconomyLog
 import java.math.BigDecimal
 
 interface TransactionLogger {
-    suspend fun logging(economyOperations: EconomyOperations, sender:String, target: String, currency: String, previousBalance: BigDecimal, newBalance: BigDecimal)
+    suspend fun logging(typeLogger: TypeLogger, sender:String, target: String, currency: String, previousBalance: BigDecimal, newBalance: BigDecimal)
     fun clearLogs()
     suspend fun getLog(): List<EconomyLog>
 }
