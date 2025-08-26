fbdc989e47c1db1f409ef107597c55614293eb5b
EncryptSL <rydlos123@gmail.com>
2025-08-26 03:00:26 +0200

    Updated kotlin and other stuff...

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 apply(from = "generatePaperLibrariesYaml.gradle.kts")
 
 plugins {
-    kotlin("jvm") version "2.2.0"
-    id("com.gradleup.shadow") version "9.0.0-beta17"
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
+    kotlin("jvm") version "2.2.10"
+    id("com.gradleup.shadow") version "9.0.2"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
 }
 
 group = "com.github.encryptsl"
@@ -37,29 +37,37 @@ kotlin {
 }
 
 dependencies {
+    // Minecraft / Paper
     paperweight.paperDevBundle(providers.gradleProperty("server_version").get())
+
+    // API & plugins
+    compileOnly(libs.placeholderapi)
     compileOnly(libs.vaultapi) {
         exclude("org.bukkit", "bukkit")
     }
-    compileOnly(libs.placeholderapi)
+    compileOnly(libs.vaultunlocked)
 
+    // Databases & migrations
+    compileOnly(libs.hikaricp)
     compileOnly(libs.exposed.core)
     compileOnly(libs.exposed.jdbc)
     compileOnly(libs.exposed.kotlin.datetime)
+    compileOnly(libs.mariadb)
+    compileOnly(libs.postqresql)
+    compileOnly(libs.sqlite)
 
-    compileOnly(libs.hikaricp)
+    // ⚠️ Flyway
     compileOnly(libs.flyway.core)
+    compileOnly(libs.flyway.mysql) // pokud používáš MySQL/MariaDB
 
-    compileOnly(libs.vaultunlocked)
+    // Kotlin
+    compileOnly(libs.kotlin.reflection)
+    compileOnly(libs.kotlin.stdlib.jdk8)
 
-    compileOnly(libs.okhttp)
-    compileOnly(libs.config.updater)
-    compileOnly(libs.commons.csv)
-
+    // Cloud Command Framework
     compileOnly(libs.cloud.annotations) {
         exclude("org.incendo", "cloud-core")
     }
-    compileOnly(libs.cloud.paper)
     compileOnly(libs.cloud.extras) {
         exclude("org.incendo", "cloud-core")
         exclude("net.kyori", "adventure-text-api")
@@ -67,30 +75,38 @@ dependencies {
         exclude("net.kyori", "adventure-text-serializer-plain")
     }
     compileOnly(libs.cloud.kotlin)
+    compileOnly(libs.cloud.paper)
 
+    // Misc
+    compileOnly(libs.commons.csv)
+    compileOnly(libs.config.updater)
+    compileOnly(libs.ktor.client.core)
+
+    // Economy plugins
     compileOnly(libs.bettereconomy)
     compileOnly(libs.scruffyeconomy)
     compileOnly(libs.craftconomy)
 
-    runtimeOnly(libs.mariadb)
-    runtimeOnly(libs.flyway.mysql)
-    runtimeOnly(libs.postqresql)
-    runtimeOnly(libs.kotlin.stdlib.jdk8)
-
+    // Implementations
     implementation(libs.bstats)
     implementation(libs.miniplaceholders)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.java)
+    implementation(libs.ktor.serialization.gson)
 
+
+    // Test
     testImplementation(libs.junit.jupiter)
-    testRuntimeOnly(libs.junit.launcher)
-
-    testImplementation(libs.mariadb)
-    testImplementation(libs.flyway.core.test)
-    testImplementation(libs.hikaricp)
-    testImplementation(libs.sqlite)
     testImplementation(libs.exposed.core)
     testImplementation(libs.exposed.jdbc)
-    testImplementation(libs.exposed.migration)
     testImplementation(libs.exposed.kotlin.datetime)
+    testImplementation(libs.exposed.migration)
+    testImplementation(libs.hikaricp)
+    testImplementation(libs.mariadb)
+    testImplementation(libs.sqlite)
+
+    testRuntimeOnly(libs.junit.launcher)
+    testImplementation(libs.flyway.core.test)
 }
 
 sourceSets {
@@ -116,9 +132,10 @@ tasks {
         archiveFileName.set("${providers.gradleProperty("plugin_name").get()}-${project.version}.jar")
         minimize {
             relocate("org.bstats", "com.github.encryptsl.metrics")
+            relocate("io.ktor", "com.github.encryptsl.ktor")
         }
         exclude("kotlin/**")
-        exclude("kotlinx/**")
+        exclude("sqlite/**")
     }
     test {
         useJUnitPlatform()
