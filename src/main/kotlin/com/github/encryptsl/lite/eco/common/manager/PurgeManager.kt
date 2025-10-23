ebd4cf268ac11512a506401d52af7f31f5fabca3
EncryptSL <rydlos123@gmail.com>
2025-10-23 09:43:13 +0200

    Improvement for BukkitAPI.

--- src/main/kotlin/com/github/encryptsl/lite/eco/common/manager/PurgeManager.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/common/manager/PurgeManager.kt
@@ -2,6 +2,7 @@ package com.github.encryptsl.lite.eco.common.manager
 
 import com.github.encryptsl.lite.eco.LiteEco
 import com.github.encryptsl.lite.eco.api.enums.PurgeKey
+import com.github.encryptsl.lite.eco.common.extensions.safeSendMessage
 import kotlinx.coroutines.launch
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import org.bukkit.command.CommandSender
@@ -14,26 +15,26 @@ class PurgeManager(private val liteEco: LiteEco) {
             when (purgeKey) {
                 PurgeKey.ACCOUNTS -> {
                     liteEco.api.purgeAccounts(currency)
-                    sender.sendMessage(liteEco.locale.translation("messages.admin.purge_accounts"))
+                    sender.safeSendMessage(liteEco, liteEco.locale.translation("messages.admin.purge_accounts"))
                 }
                 PurgeKey.NULL_ACCOUNTS -> {
                     liteEco.api.purgeInvalidAccounts(currency)
-                    sender.sendMessage(liteEco.locale.translation("messages.admin.purge_null_accounts"))
+                    sender.safeSendMessage(liteEco, liteEco.locale.translation("messages.admin.purge_null_accounts"))
                 }
                 PurgeKey.DEFAULT_ACCOUNTS -> {
                     liteEco.api.purgeDefaultAccounts(currency,liteEco.currencyImpl.getCurrencyStartBalance(currency))
-                    sender.sendMessage(liteEco.locale.translation("messages.admin.purge_default_accounts"))
+                    sender.safeSendMessage(liteEco, liteEco.locale.translation("messages.admin.purge_default_accounts"))
                 }
                 PurgeKey.MONO_LOG -> {
                     val monolog = liteEco.loggerModel.getLog()
                     if (monolog.isEmpty()) {
-                        return@launch sender.sendMessage(liteEco.locale.translation("messages.error.purge_monolog_fail"))
+                        return@launch sender.safeSendMessage(liteEco, liteEco.locale.translation("messages.error.purge_monolog_fail"))
                     }
                     liteEco.loggerModel.clearLogs()
-                    sender.sendMessage(liteEco.locale.translation("messages.admin.purge_monolog_success", Placeholder.parsed("deleted", monolog.size.toString())))
+                    sender.safeSendMessage(liteEco, liteEco.locale.translation("messages.admin.purge_monolog_success", Placeholder.parsed("deleted", monolog.size.toString())))
                 }
                 else -> {
-                    sender.sendMessage(liteEco.locale.translation("messages.error.purge_argument"))
+                    sender.safeSendMessage(liteEco, liteEco.locale.translation("messages.error.purge_argument"))
                 }
             }
         }
