664e06966f15be1317181ecae1155fb644693ef4
EncryptSL <rydlos123@gmail.com>
2026-01-02 16:39:39 +0100

    Renamed from User to UserEntity

--- src/main/kotlin/com/github/encryptsl/lite/eco/common/database/entity/UserEntity.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/common/database/entity/UserEntity.kt
@@ -3,7 +3,7 @@ package com.github.encryptsl.lite.eco.common.database.entity
 import java.math.BigDecimal
 import java.util.*
 
-data class User(
+data class UserEntity(
     val userName: String,
     val uuid: UUID,
     val money: BigDecimal
