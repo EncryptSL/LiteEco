b9ca5b966d623beb75351e524c6386ab1712f398
EncryptSL <rydlos123@gmail.com>
2026-01-27 11:36:26 +0100

    Replaced componentLogger to logger because is override in main class

--- src/main/kotlin/com/github/encryptsl/lite/eco/common/database/SqlPluginLogger.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/common/database/SqlPluginLogger.kt
@@ -11,6 +11,6 @@ import org.jetbrains.exposed.v1.jdbc.transactions.TransactionManager
 class SqlPluginLogger : SqlLogger {
     override fun log(context: StatementContext, transaction: Transaction) {
         if (!LiteEco.instance.config.getBoolean("database.sql-plugin-logger")) return
-        LiteEco.instance.componentLogger.info(ModernText.miniModernText("<gold>${context.expandArgs(TransactionManager.current())}"))
+        LiteEco.instance.logger.info(ModernText.miniModernText("<gold>${context.expandArgs(TransactionManager.current())}"))
     }
 }
