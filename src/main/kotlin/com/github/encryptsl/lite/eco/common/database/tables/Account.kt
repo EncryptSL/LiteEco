de1da6ff3c4da9935c2f7ba7ddb038836f2e345d
EncryptSL <rydlos123@gmail.com>
2026-01-27 11:35:46 +0100

    Added required OptIn annotation

--- src/main/kotlin/com/github/encryptsl/lite/eco/common/database/tables/Account.kt
+++ src/main/kotlin/com/github/encryptsl/lite/eco/common/database/tables/Account.kt
@@ -1,8 +1,10 @@
 package com.github.encryptsl.lite.eco.common.database.tables
 
 import org.jetbrains.exposed.v1.core.dao.id.LongIdTable
+import kotlin.uuid.ExperimentalUuidApi
 
 
+@OptIn(ExperimentalUuidApi::class)
 class Account(currency: String) : LongIdTable("lite_eco_$currency") {
     val username = varchar("username", 36)
     val uuid = uuid("uuid").uniqueIndex("idx_${currency}_uuid")
